fd9babc815cbeb76c683ab8d9ad67ea0a2b8f355
Hayden Roszell <hroszell@keyfactor.com>
2023-03-24 10:44:08 -0700

    Read K8s secret data, test cases

--- go.mod
+++ go.mod
@@ -3,10 +3,13 @@ module github.com/Keyfactor/ejbca-issuer
 go 1.19
 
 require (
-	github.com/Keyfactor/ejbca-go-client-sdk v0.1.2
+	github.com/Keyfactor/ejbca-go-client-sdk v0.1.4
 	github.com/cert-manager/cert-manager v1.11.0
+	github.com/go-logr/logr v1.2.3
 	github.com/onsi/ginkgo/v2 v2.6.1
 	github.com/onsi/gomega v1.24.2
+	github.com/stretchr/testify v1.8.2
+	k8s.io/api v0.26.1
 	k8s.io/apimachinery v0.26.3
 	k8s.io/client-go v0.26.1
 	k8s.io/utils v0.0.0-20221128185143-99ec85e7a448
@@ -14,13 +17,17 @@ require (
 )
 
 require (
+	github.com/Azure/go-ntlmssp v0.0.0-20220621081337-cb9428e4ac1e // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/blang/semver/v4 v4.0.0 // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/emicklei/go-restful/v3 v3.9.0 // indirect
+	github.com/evanphx/json-patch v5.6.0+incompatible // indirect
 	github.com/evanphx/json-patch/v5 v5.6.0 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
-	github.com/go-logr/logr v1.2.3 // indirect
+	github.com/go-asn1-ber/asn1-ber v1.5.4 // indirect
+	github.com/go-ldap/ldap/v3 v3.4.4 // indirect
 	github.com/go-logr/zapr v1.2.3 // indirect
 	github.com/go-openapi/jsonpointer v0.19.5 // indirect
 	github.com/go-openapi/jsonreference v0.20.0 // indirect
@@ -41,6 +48,7 @@ require (
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/prometheus/client_golang v1.14.0 // indirect
 	github.com/prometheus/client_model v0.3.0 // indirect
 	github.com/prometheus/common v0.37.0 // indirect
@@ -49,6 +57,7 @@ require (
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
 	go.uber.org/zap v1.24.0 // indirect
+	golang.org/x/crypto v0.5.0 // indirect
 	golang.org/x/net v0.7.0 // indirect
 	golang.org/x/oauth2 v0.4.0 // indirect
 	golang.org/x/sys v0.5.0 // indirect
@@ -61,7 +70,6 @@ require (
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
-	k8s.io/api v0.26.1 // indirect
 	k8s.io/apiextensions-apiserver v0.26.1 // indirect
 	k8s.io/component-base v0.26.1 // indirect
 	k8s.io/klog/v2 v2.80.1 // indirect
--- internal/issuer/signer/signer.go
+++ internal/issuer/signer/signer.go
@@ -1,184 +1,295 @@
+/*
+Copyright 2023 Keyfactor.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package signer
 
 import (
-	"context"
-	"crypto/x509"
-	"encoding/base64"
-	"encoding/pem"
-	"errors"
-	"github.com/Keyfactor/ejbca-go-client-sdk/api/ejbca"
-	ejbcaissuer "github.com/Keyfactor/ejbca-issuer/api/v1alpha1"
-	"math/rand"
-	"strings"
-	"time"
+    "context"
+    "crypto/tls"
+    "crypto/x509"
+    "encoding/base64"
+    "encoding/pem"
+    "errors"
+    "github.com/Keyfactor/ejbca-go-client-sdk/api/ejbca"
+    ejbcaissuer "github.com/Keyfactor/ejbca-issuer/api/v1alpha1"
+    "math/rand"
+    "strings"
+    "time"
 )
 
 type HealthChecker interface {
-	Check() error
+    Check() error
 }
 
 type HealthCheckerBuilder func(*ejbcaissuer.IssuerSpec, map[string][]byte) (HealthChecker, error)
 
 type Signer interface {
-	Sign([]byte) ([]byte, error)
+    Sign([]byte) ([]byte, error)
 }
 
-type Builder func(*ejbcaissuer.IssuerSpec, map[string][]byte) (Signer, error)
+type EjbcaSignerBuilder func(*ejbcaissuer.IssuerSpec, map[string][]byte) (Signer, error)
 
 func EjbcaHealthCheckerFromIssuerAndSecretData(*ejbcaissuer.IssuerSpec, map[string][]byte) (HealthChecker, error) {
-	return &ejbcaSigner{}, nil
+    return &ejbcaSigner{}, nil
 }
 
-func EjbcaSignerFromIssuerAndSecretData(*ejbcaissuer.IssuerSpec, map[string][]byte) (Signer, error) {
-	return &ejbcaSigner{}, nil
+func EjbcaSignerFromIssuerAndSecretData(_ *ejbcaissuer.IssuerSpec, secretData map[string][]byte) (Signer, error) {
+    signer := ejbcaSigner{}
+    // secretData contains the following keys:
+    // - hostname
+    // - clientCert.pem
+    // - clientKey.pem
+    // - certificateProfileName
+    // - endEntityProfileName
+    // - certificateAuthorityName
+
+    // Create EJBCA API Client
+    ejbcaConfig := ejbca.NewConfiguration()
+
+    if ejbcaConfig.Host == "" {
+        hostname, ok := secretData["hostname"]
+        if !ok {
+            return nil, errors.New("hostname not found in secret data or environment")
+        }
+        ejbcaConfig.Host = string(hostname)
+    }
+
+    clientCertByte, ok := secretData["clientCert.pem"]
+    if !ok {
+        return nil, errors.New("clientCert.pem not found in secret data")
+    }
+
+    // Decode client certificate PEM block
+    clientCertPemBlock, clientKeyBytes, err := decodePEMBytes(clientCertByte)
+    if err != nil {
+        return nil, err
+    }
+
+    // Determine if ejbcaCert contains a private key
+    clientCertContainsKey := false
+    if len(clientKeyBytes) > 0 {
+        clientCertContainsKey = true
+    }
+
+    if !clientCertContainsKey {
+        clientKeyBytes, ok = secretData["clientKey.pem"]
+        if !ok {
+            return nil, errors.New("clientKey.pem not found in secret data")
+        }
+    }
+
+    // Create a TLS certificate object
+    tlsCert, err := tls.X509KeyPair(pem.EncodeToMemory(clientCertPemBlock[0]), clientKeyBytes)
+    if err != nil {
+        return nil, err
+    }
+
+    // Add the TLS certificate to the EJBCA configuration
+    ejbcaConfig.SetClientCertificate(&tlsCert)
+
+    // Create EJBCA API Client
+    client, err := ejbca.NewAPIClient(ejbcaConfig)
+    if err != nil {
+        return nil, err
+    }
+    signer.client = client
+
+    // Extract EJBCA signing metadata from secret
+    certificateProfileName, ok := secretData["certificateProfileName"]
+    if !ok {
+        return nil, errors.New("certificateProfileName not found in secret data")
+    }
+    endEntityProfileName, ok := secretData["endEntityProfileName"]
+    if !ok {
+        return nil, errors.New("endEntityProfileName not found in secret data")
+    }
+    certificateAuthorityName, ok := secretData["certificateAuthorityName"]
+    if !ok {
+        return nil, errors.New("certificateAuthorityName not found in secret data")
+    }
+    signer.certificateProfileName = string(certificateProfileName)
+    signer.endEntityProfileName = string(endEntityProfileName)
+    signer.certificateAuthorityName = string(certificateAuthorityName)
+
+    return &signer, nil
 }
 
 type ejbcaSigner struct {
-	client                   *ejbca.APIClient
-	certificateProfileName   string
-	endEntityProfileName     string
-	certificateAuthorityName string
+    client                   *ejbca.APIClient
+    certificateProfileName   string
+    endEntityProfileName     string
+    certificateAuthorityName string
 }
 
 func (s *ejbcaSigner) Check() error {
-	return nil
+    return nil
 }
 
 func (s *ejbcaSigner) Sign(csrBytes []byte) ([]byte, error) {
-	csr, err := parseCSR(csrBytes)
-	if err != nil {
-		return nil, err
-	}
+    csr, err := parseCSR(csrBytes)
+    if err != nil {
+        return nil, err
+    }
 
-	// If the CSR has a CommonName, use it as the EJBCA end entity name
-	var ejbcaEeName string
-	if csr.Subject.CommonName != "" {
-		ejbcaEeName = csr.Subject.CommonName
-	} else {
-		ejbcaEeName = csr.Subject.SerialNumber
-	}
+    // If the CSR has a CommonName, use it as the EJBCA end entity name
+    var ejbcaEeName string
+    if csr.Subject.CommonName != "" {
+        ejbcaEeName = csr.Subject.CommonName
+    } else {
+        ejbcaEeName = csr.Subject.SerialNumber
+    }
 
-	// Configure EJBCA PKCS#10 request
-	enroll := ejbca.EnrollCertificateRestRequest{
-		CertificateRequest:       ptr(string(csrBytes)),
-		CertificateProfileName:   ptr(s.certificateProfileName),
-		EndEntityProfileName:     ptr(s.endEntityProfileName),
-		CertificateAuthorityName: ptr(s.certificateAuthorityName),
-		Username:                 ptr(ejbcaEeName),
-		Password:                 ptr(generateRandomString(20)),
-		IncludeChain:             ptr(true),
-	}
+    // Configure EJBCA PKCS#10 request
+    enroll := ejbca.EnrollCertificateRestRequest{
+        CertificateRequest:       ptr(string(csrBytes)),
+        CertificateProfileName:   ptr(s.certificateProfileName),
+        EndEntityProfileName:     ptr(s.endEntityProfileName),
+        CertificateAuthorityName: ptr(s.certificateAuthorityName),
+        Username:                 ptr(ejbcaEeName),
+        Password:                 ptr(generateRandomString(20)),
+        IncludeChain:             ptr(true),
+    }
 
-	// Enroll certificate
-	certificateObject, _, err := s.client.V1CertificateApi.EnrollPkcs10Certificate(context.Background()).EnrollCertificateRestRequest(enroll).Execute()
-	if err != nil {
-		return nil, err
-	}
+    // Enroll certificate
+    certificateObject, _, err := s.client.V1CertificateApi.EnrollPkcs10Certificate(context.Background()).EnrollCertificateRestRequest(enroll).Execute()
+    if err != nil {
+        return nil, err
+    }
 
-	certAndChain, _, err := getCertificatesFromEjbcaObject(*certificateObject)
-	if err != nil {
-		return nil, err
-	}
+    certAndChain, _, err := getCertificatesFromEjbcaObject(*certificateObject)
+    if err != nil {
+        return nil, err
+    }
 
-	// Return the certificate and chain in PEM format
-	return []byte(compileCertificatesToPemString(certAndChain)), nil
+    // Return the certificate and chain in PEM format
+    return []byte(compileCertificatesToPemString(certAndChain)), nil
 }
 
 func parseCSR(pemBytes []byte) (*x509.CertificateRequest, error) {
-	// extract PEM from request object
-	block, _ := pem.Decode(pemBytes)
-	if block == nil || block.Type != "CERTIFICATE REQUEST" {
-		return nil, errors.New("PEM block type must be CERTIFICATE REQUEST")
-	}
-	return x509.ParseCertificateRequest(block.Bytes)
+    // extract PEM from request object
+    block, _ := pem.Decode(pemBytes)
+    if block == nil || block.Type != "CERTIFICATE REQUEST" {
+        return nil, errors.New("PEM block type must be CERTIFICATE REQUEST")
+    }
+    return x509.ParseCertificateRequest(block.Bytes)
 }
 
 func getCertificatesFromEjbcaObject(ejbcaCert ejbca.CertificateRestResponse) ([]*x509.Certificate, bool, error) {
-	var certBytes []byte
-	var err error
-	certChainFound := false
+    var certBytes []byte
+    var err error
+    certChainFound := false
 
-	if ejbcaCert.GetResponseFormat() == "PEM" {
-		// Extract the certificate from the PEM string
-		block, _ := pem.Decode([]byte(ejbcaCert.GetCertificate()))
-		if block == nil {
-			return nil, false, errors.New("failed to parse certificate PEM")
-		}
-		certBytes = block.Bytes
-	} else if ejbcaCert.GetResponseFormat() == "DER" {
-		// Depending on how the EJBCA API was called, the certificate will either be single b64 encoded or double b64 encoded
-		// Try to decode the certificate twice, but don't exit if we fail here. The certificate is decoded later which
-		// will give more insight into the failure.
-		bytes := []byte(ejbcaCert.GetCertificate())
-		for i := 0; i < 2; i++ {
-			var tempBytes []byte
-			tempBytes, err = base64.StdEncoding.DecodeString(string(bytes))
-			if err == nil {
-				bytes = tempBytes
-			}
-		}
-		certBytes = append(certBytes, bytes...)
+    if ejbcaCert.GetResponseFormat() == "PEM" {
+        // Extract the certificate from the PEM string
+        block, _ := pem.Decode([]byte(ejbcaCert.GetCertificate()))
+        if block == nil {
+            return nil, false, errors.New("failed to parse certificate PEM")
+        }
+        certBytes = block.Bytes
+    } else if ejbcaCert.GetResponseFormat() == "DER" {
+        // Depending on how the EJBCA API was called, the certificate will either be single b64 encoded or double b64 encoded
+        // Try to decode the certificate twice, but don't exit if we fail here. The certificate is decoded later which
+        // will give more insight into the failure.
+        bytes := []byte(ejbcaCert.GetCertificate())
+        for i := 0; i < 2; i++ {
+            var tempBytes []byte
+            tempBytes, err = base64.StdEncoding.DecodeString(string(bytes))
+            if err == nil {
+                bytes = tempBytes
+            }
+        }
+        certBytes = append(certBytes, bytes...)
 
-		// If the certificate chain is present, append it to the certificate bytes
-		if len(ejbcaCert.GetCertificateChain()) > 0 {
-			var chainCertBytes []byte
+        // If the certificate chain is present, append it to the certificate bytes
+        if len(ejbcaCert.GetCertificateChain()) > 0 {
+            var chainCertBytes []byte
 
-			certChainFound = true
-			for _, chainCert := range ejbcaCert.GetCertificateChain() {
-				// Depending on how the EJBCA API was called, the certificate will either be single b64 encoded or double b64 encoded
-				// Try to decode the certificate twice, but don't exit if we fail here. The certificate is decoded later which
-				// will give more insight into the failure.
-				for i := 0; i < 2; i++ {
-					var tempBytes []byte
-					tempBytes, err = base64.StdEncoding.DecodeString(chainCert)
-					if err == nil {
-						chainCertBytes = tempBytes
-					}
-				}
+            certChainFound = true
+            for _, chainCert := range ejbcaCert.GetCertificateChain() {
+                // Depending on how the EJBCA API was called, the certificate will either be single b64 encoded or double b64 encoded
+                // Try to decode the certificate twice, but don't exit if we fail here. The certificate is decoded later which
+                // will give more insight into the failure.
+                for i := 0; i < 2; i++ {
+                    var tempBytes []byte
+                    tempBytes, err = base64.StdEncoding.DecodeString(chainCert)
+                    if err == nil {
+                        chainCertBytes = tempBytes
+                    }
+                }
 
-				certBytes = append(certBytes, chainCertBytes...)
-			}
-		}
-	} else {
-		return nil, false, errors.New("ejbca returned unknown certificate format: " + ejbcaCert.GetResponseFormat())
-	}
+                certBytes = append(certBytes, chainCertBytes...)
+            }
+        }
+    } else {
+        return nil, false, errors.New("ejbca returned unknown certificate format: " + ejbcaCert.GetResponseFormat())
+    }
 
-	certs, err := x509.ParseCertificates(certBytes)
-	if err != nil {
-		return nil, false, err
-	}
+    certs, err := x509.ParseCertificates(certBytes)
+    if err != nil {
+        return nil, false, err
+    }
 
-	return certs, certChainFound, nil
+    return certs, certChainFound, nil
 }
 
 // compileCertificatesToPemString takes a slice of x509 certificates and returns a string containing the certificates in PEM format
 // If an error occurred, the function logs the error and continues to parse the remaining objects.
 func compileCertificatesToPemString(certificates []*x509.Certificate) string {
-	var pemBuilder strings.Builder
+    var pemBuilder strings.Builder
 
-	for _, certificate := range certificates {
-		err := pem.Encode(&pemBuilder, &pem.Block{
-			Type:  "CERTIFICATE",
-			Bytes: certificate.Raw,
-		})
-		if err != nil {
-			// TODO logging
-		}
-	}
+    for _, certificate := range certificates {
+        err := pem.Encode(&pemBuilder, &pem.Block{
+            Type:  "CERTIFICATE",
+            Bytes: certificate.Raw,
+        })
+        if err != nil {
+            // TODO logging
+        }
+    }
 
-	return pemBuilder.String()
+    return pemBuilder.String()
+}
+
+func decodePEMBytes(buf []byte) ([]*pem.Block, []byte, error) {
+    var privKey []byte
+    var certificates []*pem.Block
+    var block *pem.Block
+    for {
+        block, buf = pem.Decode(buf)
+        if block == nil {
+            break
+        } else if strings.Contains(block.Type, "PRIVATE KEY") {
+            privKey = pem.EncodeToMemory(block)
+        } else {
+            certificates = append(certificates, block)
+        }
+    }
+    return certificates, privKey, nil
 }
 
 func ptr[T any](v T) *T {
-	return &v
+    return &v
 }
 
 func generateRandomString(length int) string {
-	rand.Seed(time.Now().UnixNano())
-	letters := []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
-	b := make([]rune, length)
-	for i := range b {
-		b[i] = letters[rand.Intn(len(letters))]
-	}
-	return string(b)
+    rand.Seed(time.Now().UnixNano())
+    letters := []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    b := make([]rune, length)
+    for i := range b {
+        b[i] = letters[rand.Intn(len(letters))]
+    }
+    return string(b)
 }
